277acb3e915cff89b75281b7e69bdb8d2fe02915
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2019-01-04 16:47:45 +0100

    [2T1R-PKM] Datei mit Strukturparametern erstellt. Dadurch ist die Dimension der Eingabevariablen für die Funktion "JInv" automatisiert feststellbar.

---
+++ sym3leg/P3PRP1/hd_A2/P3PRP1A2_structural_kinematic_parameters.m
@@ -0,0 +1,17 @@
+% Return Structural Kinematic Parameters of the parallel Robot 
+% P3PPR2A1
+%
+% Output:
+% NQJ_leg [1x1]
+%   Anzahl der verwendeten Gelenkkoordinaten der symmetrischen Beinsegmente.
+%   Von der Basis aus gezählt
+
+% Quelle: HybrDyn-Toolbox
+% Datum: 2019-01-03 13:46
+% Revision: 73fedb8d52c8ee36c720354c027a250325027d46 (2019-01-03)
+% Moritz Schappler, moritz.schappler@imes.uni-hannover.de
+% (C) Institut für Mechatronische Systeme, Universität Hannover
+
+function [NQJ_leg] = P3PPR2A1_structural_kinematic_parameters()
+
+NQJ_leg = 3;
