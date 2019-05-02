a7ff34cb237f0ff9efc0bb87879500b3c5231d99
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2019-05-02 14:09:27 +0100

    [P6RRPRRR14V3] Dynamik-Funktionen für 6UPS-Roboter generiert (aus Varianten-Modell der seriellen Beinkette mit minimalen Parametern)

---
+++ sym6leg/P6RRPRRR14V3/hd_A0/P6RRPRRR14V3A0_minimal_parameter_para.m
@@ -0,0 +1,55 @@
+% Return the minimum parameter vector for
+% P6RRPRRR14V3A0
+% Use Code from Maple symbolic Code Generation
+% 
+% Input:
+% pkin [1x1]
+%   kinematic parameters (e.g. lengths of the links)
+%   pkin=[dummy]';
+% m [4x1]
+%   mass of all robot links (leg links until cut joint, platform)
+% mrSges [4x3]
+%   first moment of all robot links (mass times center of mass in body frames)
+%   rows: links of the robot (leg links until cut joint, platform)
+%   columns: x-, y-, z-coordinates
+% Ifges [4x6]
+%   inertia of all robot links about their respective body frame origins, in body frames
+%   rows: links of the robot (leg links until cut joint, platform)
+%   columns: xx, yy, zz, xy, xz, yz (see inertial_parameters_convert_par1_par2.m)
+% koppelP [6x3]
+%   coordinates of the platform coupling joints
+%   (joints that link the end of legs with platform)
+%   in platform coordinates
+%   rows: number of leg
+%   columns: x-, y-, z-coordinates
+% 
+% Output:
+% MPV [24x1]
+%   base parameter vector (minimal parameter vector)
+
+% Quelle: HybrDyn-Toolbox
+% Datum: 2019-04-15 09:53
+% Revision: 3acd05283b8979b361f80d69cfa1c98d98241298 (2019-04-12)
+% Moritz Schappler, moritz.schappler@imes.uni-hannover.de
+% (C) Institut für Mechatronische Systeme, Universität Hannover
+
+function MPV = P6RRPRRR14V3A0_minimal_parameter_para(pkin, m, mrSges, Ifges, koppelP)
+
+%% Coder Information
+%#codegen
+%$cgargs {zeros(1,1),zeros(3+1,1),zeros(3+1,3),zeros(3+1,6),zeros(6,3)}
+assert(isreal(pkin) && all(size(pkin) == [1 1]), ...
+  'P6RRPRRR14V3A0_minimal_parameter_para: pkin has to be [1x1] (double)');
+assert(isreal(m) && all(size(m) == [4 1]), ...
+  'P6RRPRRR14V3A0_minimal_parameter_para: m has to be [4x1] (double)'); 
+assert(isreal(mrSges) && all(size(mrSges) == [4,3]), ...
+  'P6RRPRRR14V3A0_minimal_parameter_para: mrSges has to be [4x3] (double)');
+assert(isreal(Ifges) && all(size(Ifges) == [4 6]), ...
+  'P6RRPRRR14V3A0_minimal_parameter_para: Ifges has to be [4x6] (double)'); 
+assert(isreal(koppelP) && all(size(koppelP) == [6 3]), ...
+  'P6RRPRRR14V3A0_minimal_parameter_para: Koppelpunkt has to be [6x3] (double)');
+
+%% Symbolic Calculation
+% From minimal_parameter_parrob_matlab.m
+t1 = [Ifges(1,3) + Ifges(2,2) + Ifges(3,3); mrSges(1,1); mrSges(1,2) - mrSges(2,3); Ifges(2,1) + Ifges(3,1) - Ifges(2,2) - Ifges(3,3); Ifges(2,4) - Ifges(3,5); Ifges(2,5) + Ifges(3,4); Ifges(2,6) - Ifges(3,6); Ifges(2,3) + Ifges(3,2); mrSges(2,1); mrSges(2,2); mrSges(3,1); mrSges(3,2); mrSges(3,3); m(3); Ifges(4,1); Ifges(4,4); Ifges(4,5); Ifges(4,2); Ifges(4,6); Ifges(4,3); mrSges(4,1); mrSges(4,2); mrSges(4,3); m(4);];
+MPV  = t1;
