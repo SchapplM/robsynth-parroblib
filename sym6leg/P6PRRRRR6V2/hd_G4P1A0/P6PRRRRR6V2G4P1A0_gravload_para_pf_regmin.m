d96eda4c70d7834a87ef2b838a11eecaa74297a6
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2019-09-11 17:27:26 +0200

    [6PUS] Umbenennung des bestehenden Modells von G1 zu G4

--- sym6leg/P6PRRRRR6V2/hd_G4P1A0/P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin.m
+++ sym6leg/P6PRRRRR6V2/hd_G4P1A0/P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin.m
@@ -1,5 +1,5 @@
 % Calculate minimal parameter regressor of Gravitation load for parallel robot
-% P6PRRRRR6V2G1P1A0
+% P6PRRRRR6V2G4P1A0
 % Use Code from Maple symbolic Code Generation
 %
 % Input:
@@ -37,23 +37,23 @@
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de
 % (C) Institut für Mechatronische Systeme, Universität Hannover
 
-function tau_reg = P6PRRRRR6V2G1P1A0_gravload_para_pf_regmin(xP, qJ, g, legFrame, ...
+function tau_reg = P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin(xP, qJ, g, legFrame, ...
   koppelP, pkin)
 %% Coder Information
 %#codegen
 %$cgargs {zeros(6,1),zeros(3,6),zeros(3,1),zeros(6,3),zeros(6,3),zeros(10,1)}
 assert(isreal(xP) && all(size(xP) == [6 1]), ...
-  'P6PRRRRR6V2G1P1A0_gravload_para_pf_regmin: xP has to be [6x1] (double)');
+  'P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin: xP has to be [6x1] (double)');
 assert(isreal(qJ) && all(size(qJ) == [3 6]), ...
-  'P6PRRRRR6V2G1P1A0_gravload_para_pf_regmin: qJ has to be [3x6] (double)');
+  'P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin: qJ has to be [3x6] (double)');
 assert(isreal(g) && all(size(g) == [3 1]), ...
-  'P6PRRRRR6V2G1P1A0_gravload_para_pf_regmin: g has to be [3x1] (double)');
+  'P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin: g has to be [3x1] (double)');
 assert(isreal(pkin) && all(size(pkin) == [10 1]), ...
-  'P6PRRRRR6V2G1P1A0_gravload_para_pf_regmin: pkin has to be [10x1] (double)');
+  'P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin: pkin has to be [10x1] (double)');
 assert(isreal(legFrame) && all(size(legFrame) == [6 3]), ...
-  'P6PRRRRR6V2G1P1A0_gravload_para_pf_regmin: legFrame has to be [6x1] (double)');
+  'P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin: legFrame has to be [6x1] (double)');
 assert(isreal(koppelP) && all(size(koppelP) == [6 3]), ...
-  'P6PRRRRR6V2G1P1A0_gravload_para_pf_regmin: Koppelpunkt has to be [6x3] (double)');
+  'P6PRRRRR6V2G4P1A0_gravload_para_pf_regmin: Koppelpunkt has to be [6x3] (double)');
 
 %% Symbolic Calculation
 % From invdyn_para_plfcoord_taugreg_matlab.m
