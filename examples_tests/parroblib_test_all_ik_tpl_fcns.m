18b9a8436ada2f034ed99a04b49e68397bd10b3d
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-07-21 09:22:14 +0200

    Testskripte an neue IK-Funktionen angepasst

--- examples_tests/parroblib_test_all_ik_tpl_fcns.m
+++ examples_tests/parroblib_test_all_ik_tpl_fcns.m
@@ -1,8 +1,10 @@
 % Aufruf der IK-Funktionen aller existierende PKM in der Bibliothek
 % Bestimme Kinematikparameter mit Maßsynthese,
 % dann IK testen, Klassenmethode gegen Templatemethode.
+% (Jede Methode ist auf zwei Arten implementiert)
 
-% Junnan Li, WiHi bei moritz.schappler@imes.uni-hannover.de 2020-06
+% Junnan Li (WiHi bei Moritz Schappler), 2020-06
+% Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2020-07
 % (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
 
 clear
@@ -11,10 +13,15 @@ clc
 %% Initialisierung
 % Schalter zur Neugenerierung der Template-Funktionen
 tpl_fcn_neu = true;
+test_mex = true; % Benutze mex-Funktionen beim Testen (schneller, aber kein Debuggen möglich)
+recompile_mex = true; % nur notwendig, falls Template-Dateien geändert wurden.
 max_num_pkm = 20; % Reduziere die Anzahl der geprüften PKM pro FG
+shuffle_pkm_selection = true; % Zufällige Auswahl der PKM
+max_single_points = 50; % Anzahl der zu prüfenden Einzelpunkte
 % Speicherort der Parameter
 rob_path = fileparts(which('robotics_toolbox_path_init.m'));
 tmpdir_params = fullfile(rob_path, 'examples_tests', 'tmp_ParRob', 'param_dimsynthres');
+mkdirs(tmpdir_params);
 % Alle FG-Kombinationen durchgehen
 EEFG_Ges = [1 1 0 0 0 1; ...
             1 1 1 0 0 0; ...
@@ -29,19 +36,23 @@ for i_FG = 1:size(EEFG_Ges,1)
     continue % Es gibt keine PKM mit diesen FG.
   end
   III = 1:length(PNames_Kin);
-  III = III(randperm(length(III))); % shuffle
-  for ii = III(1:min(max_num_pkm, length(III)))
+  if shuffle_pkm_selection
+    III = III(randperm(length(III)));
+  end
+  for ii = III(1:min(max_num_pkm, length(III))) % Debug: find(strcmp(PNames_Kin, 'P6RRPRRR14V3G1P4'));
     PName = [PNames_Kin{ii},'A1']; % Nehme nur die erste Aktuierung (ist egal)
     [~, LEG_Names, ~, ~, ~, ~, ~, ~, PName_Legs, ~] = parroblib_load_robot(PName);
     paramfile_robot = fullfile(tmpdir_params, sprintf('%s_params.mat', PName));
     fprintf('Untersuche PKM %s\n', PName);
     if tpl_fcn_neu 
-      serroblib_create_template_functions({LEG_Names{1}},false,true)
-      parroblib_create_template_functions({PNames_Kin{ii}},false,true);
+      serroblib_create_template_functions({LEG_Names{1}},false,recompile_mex)
+      parroblib_create_template_functions({PNames_Kin{ii}},false,recompile_mex);
     end
     RP = parroblib_create_robot_class(PName, 1, 0.3);
     % Initialisierung der Funktionen: Kompilierte Funktionen nehmen
-    files_missing = RP.fill_fcn_handles(true, true);
+    % (notwendig für Struktursynthese)
+    RP.fill_fcn_handles(true, true); % zur Prüfung, ob kompilierte Funktionen vorhanden sind
+    RP.fill_fcn_handles(test_mex, true); % zur Einstellung mex ja/nein
     %% Kinematikparameter durch Optimierung erzeugen (oder gespeichert laden)
     Set = cds_settings_defaults(struct('DoF', EE_FG));
     Set.task.Ts = 1e-2;
@@ -116,73 +127,170 @@ for i_FG = 1:size(EEFG_Ges,1)
     % Klassenmethode gegen Templatemethode
     s = struct('Phit_tol', 1e-9, 'Phir_tol', 1e-9, 'retry_limit', 0, ...
       'normalize', false, 'n_max', 5000);
-    % Teste inverse Kinematik für Einzelpunkte
-    for k = 1:size(Traj_0.X,1)
-      % Teste inverse Kinematik für Einzelpunkte nach erster Methode
-      [q_kls, Phi_kls, Tc_stack_kls]=RP.invkin_ser(Traj_0.X(k,:)', q0, s); % Klassen
-      [q_tpl, Phi_tpl, Tc_stack_tpl]=RP.invkin2(Traj_0.X(k,:)', q0, s); % Template
-      ik_res_ik2 = (all(abs(Phi_tpl(RP.I_constr_t_red))<s.Phit_tol) && ...
-                    all(abs(Phi_tpl(RP.I_constr_r_red))<s.Phir_tol));% IK-Status Funktionsdatei
-      ik_res_iks = (all(abs(Phi_kls(RP.I_constr_t_red))<s.Phit_tol) && ... 
-                    all(abs(Phi_kls(RP.I_constr_r_red))<s.Phir_tol)); % IK-Status Klassenmethode
-      if ik_res_ik2 ~= ik_res_iks % Vergleiche IK-Status (Erfolg / kein Erfolg)
-        error('IK Status nicht gleich');
-      elseif ~ik_res_iks  % beide IK-Status false
-        error('IK Status beide nicht erfolgreich');
+    %% Teste inverse Kinematik für Einzelpunkte nach erster Methode
+    % Wähle nur wenige Punkte der Trajektorie zum Testen der Einzelpunkt-IK
+    II_traj = 1:size(Traj_0.X,1);
+    II_traj = II_traj(randperm(length(II_traj)));
+    max_single_points = min(max_single_points, length(II_traj));
+    for jj = 1:2
+      s_jj = s;
+      % Führe die IK mehrfach mit unterschiedlichen Einstelungen durch.
+      % Dadurch werden möglichst viele unterschiedliche Testfälle in den
+      % Funktionen abgedeckt
+      switch jj
+        case 1
+          % Standard-Einstellungen
+        case 2
+          if all(EE_FG == [1 1 1 1 1 1])
+            s_jj.I_EE = logical([1 1 1 1 1 0]);
+          else
+            % 3T2R-Aufruf ergibt nur für 3T3R-PKM Sinn.
+            continue
+          end
       end
-      ik_test_Tc_stack = Tc_stack_kls - Tc_stack_tpl;
-      if max(abs(ik_test_Tc_stack(:))) > 1e-3
-        error('Ausgabe Tc_stack stimmt nicht überein');
+      calctimes = NaN(max_single_points,2); % zum Abspeichern der Rechenzeit
+      for i = 1:max_single_points
+        k = II_traj(i);
+        t1=tic();
+        [q_kls, Phi_kls, Tc_stack_kls]=RP.invkin_ser(Traj_0.X(k,:)', q0, s); % Klassen
+        calctimes(i,1)=toc(t1);
+        t1=tic();
+        [q_tpl, Phi_tpl, Tc_stack_tpl]=RP.invkin2(Traj_0.X(k,:)', q0, s); % Template
+        calctimes(i,2)=toc(t1);
+        ik_res_ik2 = (all(abs(Phi_tpl(RP.I_constr_t_red))<s.Phit_tol) && ...
+                      all(abs(Phi_tpl(RP.I_constr_r_red))<s.Phir_tol));% IK-Status Funktionsdatei
+        ik_res_iks = (all(abs(Phi_kls(RP.I_constr_t_red))<s.Phit_tol) && ... 
+                      all(abs(Phi_kls(RP.I_constr_r_red))<s.Phir_tol)); % IK-Status Klassenmethode
+        if ik_res_ik2 ~= ik_res_iks % Vergleiche IK-Status (Erfolg / kein Erfolg)
+          error('invkin_ser vs invkin2: IK Status nicht gleich');
+        elseif ~ik_res_iks  % beide IK-Status false
+          error('invkin_ser vs invkin2: IK Status beide nicht erfolgreich');
+        end
+        ik_test_Tc_stack = Tc_stack_kls - Tc_stack_tpl;
+        if max(abs(ik_test_Tc_stack(:))) > 1e-3
+          error('invkin_ser vs invkin2: Ausgabe Tc_stack stimmt nicht überein');
+        end
+        test_q = q_kls-q_tpl;
+        if any(abs(test_q)>1e-6)
+          warning('invkin_ser vs invkin2: Gelenkwinkel aus kls und tpl stimmen nicht überein');
+        end
       end
-      test_q = q_kls-q_tpl;
-      if any(abs(test_q)>1e-6)
-        warning('Gelenkwinkel aus kls und tpl stimmen nicht überein');
+      fprintf(['Klassen- und Template-Methode für Einzelpunkt-IK stimmen überein (Fall %d, %d Punkte).\n', ...
+        'Zeiten: invkin_ser: mean %1.1fms (std %1.1fms), invkin2: mean %1.1fms (std %1.1fms)\n'], ...
+        jj, max_single_points, 1e3*mean(calctimes(:,1)), ...
+        1e3*std(calctimes(:,1)), 1e3*mean(calctimes(:,2)), 1e3*std(calctimes(:,2)));
+    end
+    %% Teste inverse Kinematik für Einzelpunkte nach zweiter Methode
+    for jj = 1:2
+      calctimes = NaN(max_single_points,2);
+      % Geht vorerst nur für 3T3R
+      if ~all(EE_FG == [1 1 1 1 1 1])
+        break % Die folgenden Tests alle nur für 3T3R.
       end
-      % Teste inverse Kinematik für Einzelpunkte nach zweiter Methode
-      % Geht eventuell nur für 3T3R
-
-      if all(EE_FG == [1 1 1 1 1 1])
-        [q_kls_3, Phi_kls_3, Tc_stack_kls_3]=RP.invkin3(Traj_0.X(k,:)', q0, s);
-        [q_tpl_3, Phi_tpl_3, Tc_stack_tpl_3]=RP.invkin4(Traj_0.X(k,:)', q0, s);
+      % Standard-FG-Auswahl
+      RP.update_EE_FG(logical([1 1 1 1 1 1]), logical([1 1 1 1 1 1]));
+      s_jj = s;
+      switch jj
+        case 1
+          % Teste IK mit vollständigen Freiheitsgraden
+          % Standard-Einstellungen: 3T3R-IK
+        case 2
+          % Teste IK mit Aufgabenredundanz
+          % 3T2R-IK
+          s_jj.I_EE = logical([1 1 1 1 1 0]);
+          RP.update_EE_FG(logical([1 1 1 1 1 1]), logical([1 1 1 1 1 0]));
+      end
+      for i = 1:max_single_points
+        k = II_traj(i);
+        t1=tic();
+        [q_kls_3, Phi_kls_3, Tc_stack_kls_3]=RP.invkin3(Traj_0.X(k,:)', q0, s_jj);
+        calctimes(i,1)=toc(t1);
+        t1=tic();
+        [q_tpl_3, Phi_tpl_3, Tc_stack_tpl_3]=RP.invkin4(Traj_0.X(k,:)', q0, s_jj);
+        calctimes(i,2)=toc(t1);
         ik_res2_ik2 = (all(abs(Phi_tpl_3(RP.I_constr_t_red))<s.Phit_tol) && ...
           all(abs(Phi_tpl_3(RP.I_constr_r_red))<s.Phir_tol));% IK-Status Funktionsdatei
         ik_res2_iks = (all(abs(Phi_kls_3(RP.I_constr_t_red))<s.Phit_tol) && ...
           all(abs(Phi_kls_3(RP.I_constr_r_red))<s.Phir_tol)); % IK-Status Klassenmethode
         if ik_res2_ik2 ~= ik_res2_iks % Vergleiche IK-Status (Erfolg / kein Erfolg)
-          error('IK Status nicht gleich');
+          error('invkin3 vs invkin4: IK Status nicht gleich');
         elseif ~ik_res2_iks  % beide IK-Status false
-          error('IK Status beide nicht erfolgreich');
+          % Gebe keinen Fehler aus. In der Struktursynthese wird die
+          % Einzelpunkt-IK nur für die Eckpunkte benutzt.
+          warning(['invkin3 vs invkin4: Pose %d/%d: IK Status beide nicht ', ...
+            'erfolgreich (Fall %d)'], k, size(Traj_0.X,1), jj);
         end
         ik_test_Tc_stack_2 = Tc_stack_kls_3 - Tc_stack_tpl_3;
-        if max(abs(ik_test_Tc_stack_2(:))) > 1e-3
-          error('Ausgabe Tc_stack stimmt nicht überein');
+        if max(abs(ik_test_Tc_stack_2(:))) > 5e-2
+          warning(['invkin3 vs invkin4: Ausgabe Tc_stack stimmt nicht ', ...
+            'überein. Max. Abweichung %1.1e'], max(abs(ik_test_Tc_stack_2(:))));
         end
         test_q_2 = q_kls_3-q_tpl_3;
-        if any(abs(test_q_2)>1e-6)
-          warning('Gelenkwinkel aus kls und tpl stimmen nicht überein');
-        end
+        if any(abs(test_q_2)>5e-2)
+          warning(['invkin3 vs invkin4: Gelenkwinkel aus kls und tpl stimmen ', ...
+            'nicht überein. Max. Abweichung %1.1e'], max(abs(test_q_2)));
+        end        
       end
+      fprintf(['Klassen- und Template-Methode für Einzelpunkt-IK Variante 2 stimmen überein (Fall %d, %d Punkte).\n', ...
+        'Zeiten: invkin3: mean %1.1fms (std %1.1fms), invkin4: mean %1.1fms (std %1.1fms)\n'], ...
+        jj, max_single_points, 1e3*mean(calctimes(:,1)), 1e3*std(calctimes(:,1)), 1e3*mean(calctimes(:,2)), 1e3*std(calctimes(:,2)));
     end
-    s_traj_ik = s;
-    % Teste inverse Kinematik für Trajektorie nach erster Methode
-    if all(EE_FG == [1 1 1 1 1 1])
-      ik_modes = [1 2]; % hier geht die invkin3- und invkin_ser-Methode
-    else
-      ik_modes = 1; % hier nur invkin_ser-Methode
-    end
-    for ik = ik_modes
-      s_traj_ik.mode_IK = ik;
-      [Q1_t_kls, QD1_t_kls, ~, Phi1_t_kls,~,~, JointPos_all_kls] = RP.invkin_traj(Traj_0.X, Traj_0.XD, Traj_0.XDD, Traj_0.t, q0, s_traj_ik);% Klassen
-      [Q1_t_tpl, QD1_t_tpl, ~, Phi1_t_tpl,~,~, JointPos_all_tpl] = RP.invkin2_traj(Traj_0.X, Traj_0.XD, Traj_0.XDD, Traj_0.t, q0, s_traj_ik);% Template
-      test_joint = JointPos_all_kls - JointPos_all_tpl;
-      test_Q1 = Q1_t_kls - Q1_t_tpl;
-      if any(abs(test_joint)>1e-6)
-        warning('JointPos aus kls und tpl stimmen nicht überein');
+    %% Teste inverse Kinematik für Trajektorie
+    for jj = 1:5
+      calctimes = NaN(1,2);
+      % Zurücksetzen der Aufgaben-FG auf Standard-Wert
+      RP.update_EE_FG(RP.I_EE, RP.I_EE);
+      s_jj = s;
+      switch jj
+        case 1
+          % Teste IK mit vollständigen Freiheitsgraden
+          % Standard-Einstellungen: 3T3R-IK
+          s_jj.mode_IK = 1;
+        case 2
+          % Standard-Einstellungen: 3T3R-IK
+          if all(EE_FG == [1 1 1 1 1 1])
+            s_jj.mode_IK = 2;
+          else
+            continue
+          end
+        case 3
+          % Vereinfachte Berechnung der Beschleunigung, vollständige FG
+          s_jj.simplify_acc = true;
+        case 4
+          % Teste IK mit Aufgabenredundanz
+          % 3T2R-IK
+          if ~all(EE_FG == [1 1 1 1 1 1]), continue; end
+          s_jj.mode_IK = 1;
+          s_jj.I_EE_Task = logical([1 1 1 1 1 0]);
+          RP.update_EE_FG(logical([1 1 1 1 1 1]), logical([1 1 1 1 1 0]));
+        case 5
+          % Vereinfachte Berechnung der Beschleunigung, Aufgabenredundanz
+          if ~all(EE_FG == [1 1 1 1 1 1]), continue; end
+          s_jj.simplify_acc = true;
+          s_jj.I_EE_Task = logical([1 1 1 1 1 0]);
+          RP.update_EE_FG(logical([1 1 1 1 1 1]), logical([1 1 1 1 1 0]));
       end
-      if any(abs(test_Q1)>1e-6)
-        warning('Q1 aus kls und tpl stimmen nicht überein');
+      t1=tic();
+      [Q_t_kls, QD_t_kls, ~, Phi1_t_kls,~,~, JointPos_all_kls] = ...
+        RP.invkin_traj(Traj_0.X, Traj_0.XD, Traj_0.XDD, Traj_0.t, q0, s_jj);% Klassen
+      calctimes(1)=toc(t1);
+      t1=tic();
+      [Q_t_tpl, QD_t_tpl, ~, Phi1_t_tpl,~,~, JointPos_all_tpl] = ...
+        RP.invkin2_traj(Traj_0.X, Traj_0.XD, Traj_0.XDD, Traj_0.t, q0, s_jj);% Template
+      calctimes(2)=toc(t1);
+      test_JP = JointPos_all_kls - JointPos_all_tpl;
+      test_Q = Q_t_kls - Q_t_tpl;
+      if any(abs(test_JP(:))>1e-6)
+        warning('invkin_traj vs invkin2_traj: JointPos aus kls und tpl stimmen nicht überein. Max. Abweichung %1.1e', ...
+          max(abs(test_JP(:))));
       end
+      if any(abs(test_Q(:))>1e-6)
+        warning('invkin_traj vs invkin2_traj: Q1 aus kls und tpl stimmen nicht überein. Max. Abweichung %1.1e', ...
+          max(abs(test_Q(:))));
+      end
+      fprintf(['Klassen- und Template-Methode für Traj.-IK stimmen überein (Fall %d).\n', ...
+        'Zeiten: invkin_traj: %1.2fs, invkin2_traj: %1.2fs\n'], ...
+        jj, calctimes(1), calctimes(2));
     end
-    fprintf('Klassen- und Template-Methode stimmen überein\n');
   end
 end
