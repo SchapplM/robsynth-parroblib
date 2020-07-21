708637a3978cab50f45fbb06662d7f0a348d75a8
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-07-21 09:58:10 +0100

    Funktionen neu strukturiert: parroblib_create_csv in parroblib_update_csv integriert, Übergabe mit EE-FG anstatt Anzahl Beinketten

--- parroblib_update_csv.m
+++ parroblib_update_csv.m
@@ -1,9 +1,13 @@
-% csv-Datei für alle vorhanden PKM aus Datenbank erneuen
+% CSV-Datei für alle vorhanden PKM aus Datenbank erneuen
+% Erzeugt/Aktualisiert Datei synthesis_result_lists/aTbR.csv ({a,b}={0,1,2,3})
 % 
 % Eingabe:
-% NLEG: Anzahl der Beinketten
-%   3: 3T0R
-%   4: 3T1R
+% SName:
+%   Name der seriellen Führungskette (symmetrische PKM)
+% Coupling [1x2]:
+%   Nummern der Gestell- und Plattform-Koppelpunkt-Varianten
+% EE_FG0:
+%   Endeffektor-FG (Geschwindigkeitskomponenten)
 % Status: Status der PKM in csv-datei zu speichern
 %   0=in Ordnung, Rang konnte geprüft werden
 %   1=Ausschluss wegen Anzahl der Schubgelenke
@@ -12,34 +16,46 @@
 %   4=keine Lösbarkeit der Traj.-IK in Maßsynthese
 %   5=noch nicht geprueft
 % Rangerfolg: 
-%   0: Rangverlust
-%   1: Rangerfolgreich
+%   0: Rangverlust in Jacobi-Matrix. PKM verliert FG und funktioniert nicht.
+%   1: Kein Rangverlust in Jacobi-Matrix. Gültige PKM.
+% 
+% Für erstmalige Generierung der Datei aus den Dateien actuation.csv:
+% parroblib_update_csv('', [], logical([1 1 1 1 1 1]))
 
 % Junnan Li, ljn931022@gmail.com, 2020-05
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2019-07
 % (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
 
-function parroblib_update_csv(SName, Coupling, NLEG, Status, Rangerfolg)
+function parroblib_update_csv(SName, Coupling, EE_FG0, Status, Rangerfolg)
 
 %% Initialisierung
 repopath=fileparts(which('parroblib_path_init.m'));
-if nargin == 4
+if nargin <= 4
   Rangerfolg = 0;
 end
+assert(isa(EE_FG0, 'logical'), 'Eingabe EE_FG0 muss 1x6 logical sein')
 
-if NLEG == 3
+if all(EE_FG0 == logical([1 1 0 0 0 1]))
+  csvtable = fullfile(repopath, sprintf('synthesis_result_lists'), sprintf('2T1R.csv'));
+elseif all(EE_FG0 == logical([1 1 1 0 0 0]))
   csvtable = fullfile(repopath, sprintf('synthesis_result_lists'), sprintf('3T0R.csv'));
-  EE_FG0 = logical([1 1 1 0 0 0]);
-elseif NLEG == 4
+elseif all(EE_FG0 == logical([1 1 1 0 0 1]))
   csvtable = fullfile(repopath, sprintf('synthesis_result_lists'), sprintf('3T1R.csv'));
-  EE_FG0 = logical([1 1 1 0 0 1]);
+elseif all(EE_FG0 == logical([1 1 1 1 1 1]))
+  csvtable = fullfile(repopath, sprintf('synthesis_result_lists'), sprintf('3T3R.csv'));
+else
+  error('Fall noch nicht implementiert');
 end
 if ~exist(csvtable, 'file')
-  warning('csv-Datei existiert nicht. Erstelle.');
+  warning('csv-Datei existiert nicht. Erstelle: %s', csvtable);
   % Tabelle existiert nicht. Erstellen
-  parroblib_create_csv(NLEG);
+  parroblib_create_csv(EE_FG0, csvtable);
 end
-
+if isempty(SName)
+  fprintf('Keine Beinkette gegeben. Aufruf nur zum Erstellen der CSV. Abbruch.\n');
+  return
+end
+NLEG = sum(EE_FG0);
 %% PKM in csv-Datei eingeben
 T = readtable(csvtable, 'ReadVariableNames', true);
 for i = 1:size(T,1)
@@ -91,3 +107,56 @@ T_new = [T;NewRow];
 T_sort = sortrows(T_new,[1 3 4]);
 writetable(T_sort,csvtable, 'Delimiter', ';');
 
+end
+
+% csv-Datei für alle vorhanden PKM aus Datenbank erzeugen
+% 
+% Eingabe:
+% EE_FG0: Endeffektor-FG (Geschwindigkeitskomponenten)
+
+% Junnan Li, ljn931022@gmail.com, 2020-05
+% Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2019-07
+% (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
+
+function parroblib_create_csv(EE_FG0, csvtable)
+
+%% Kinematik-Tabelle durchsuchen
+EE_FG_Mask = logical([1 1 1 1 1 1]); % hat keine Wirkung
+NLEG = sum(EE_FG0);
+[PNames_Kin, PNames_Akt, AdditionalInfo_Akt] = parroblib_filter_robots( ...
+  NLEG, EE_FG0, EE_FG_Mask, 6);
+
+%% PKM von Datenbank zu csv eingeben
+tableheading = {'Beinkette','Anzahl_Beinketten','G_Methode','P_Methode','Status', 'Anzahl_Akt'};
+All_PKM = {};
+for i = 1:length(PNames_Kin)
+  % Finde alle Aktuierungen zu dieser Kinematik
+  AnzahlErfolgAkt_i = 0;
+  for j = 1:length(PNames_Akt)
+    if length(PNames_Kin{i}) > length(PNames_Akt{j})
+      % Namen passen nicht zueinander. j-Aktuierung kann nicht zu
+      % i-Kinematik passen.
+      continue
+    end
+    if strcmp( PNames_Kin{i}, PNames_Akt{j}(1:length(PNames_Kin{i})) )
+      % j-Aktuierung passt zu i-Kinematik
+      if AdditionalInfo_Akt(j) == 0 % kein Rangverlust
+        AnzahlErfolgAkt_i = AnzahlErfolgAkt_i + 1;
+      end
+    end
+  end
+  Status = 0; % Da die PKM in der Datenbank ist, konnte sie offensichtlich vorher geprüft werden.
+  [~,LEG_Names,~, Coupling] = parroblib_load_robot(sprintf('%s%s', PNames_Kin{i}, 'A1'));
+  Robcell = {LEG_Names{1},NLEG,Coupling(1),Coupling(2),Status,AnzahlErfolgAkt_i}; % PKM Zeile 
+  All_PKM = [All_PKM;Robcell]; %#ok<AGROW>
+end
+All_PKM_sort = sortrows(All_PKM,[1 3 4]);
+tabledata = cell2table(All_PKM_sort);
+tabledata.Properties.VariableNames = tableheading;
+if ~exist(csvtable, 'file')
+  % Tabelle existiert nicht. Ordner erstellen.
+  mkdirs(fileparts(csvtable));
+end
+writetable(tabledata, csvtable, 'Delimiter', ';');
+fprintf('Datei %s angelegt.\n', csvtable);
+end
