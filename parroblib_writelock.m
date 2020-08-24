1621000c7e155ffc94c6f695e9f1f98ec271cc83
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-08-24 20:52:55 +0100

    Jetzt auch Sperren der mex-Dateien für einzelne PKM anstatt aller PKM des Freiheitsgrades

--- parroblib_writelock.m
+++ parroblib_writelock.m
@@ -7,9 +7,9 @@
 % mode
 %   Modus der Schreibsperre: 'free', 'check', 'lock'
 % content
-%   Ressourcen, die geschützt werden: 'csv', 'mex'
+%   Ressourcen, die geschützt werden: 'csv', PKM-Kinematik (z.B. 'P3PRRRR7')
 %   csv beinhaltet alle Tabellen für die betroffenen Freiheitsgrade
-%   mex betrifft die Vorlagen-Funktionen und das kompilieren der Funktionen
+%   PKM betrifft das kompilieren der Funktionen für die jeweilige PKM.
 % EEFG
 %   EE-FG des Roboters im Format [1 1 1 0 0 0] (Bool)
 % patience
@@ -39,20 +39,33 @@ end
 %% Initialisierung
 folder = sprintf('sym%dleg', sum(EEFG));
 repopath = fileparts(which('parroblib_path_init.m'));
-filedir = fullfile(repopath, folder);
+% Prüfe, ob die Eingabe ein Robotername ist.
+expression_robname = 'P(\d)([RP]+)(\d+)[V]?';
+[is_robname, ~] = regexp(content,expression_robname);
 if strcmp(content, 'csv')
   filename = 'writelock_csv.lock';
+  filedir = fullfile(repopath, folder);
   lockfile = fullfile(filedir, filename);
-elseif strcmp(content, 'mex')
+  ressource_name = sprintf('%s/%dT%dR', content, sum(EEFG(1:3)), sum(EEFG(4:6)));
+elseif ~isempty(is_robname)
+  expression_robname2 = '(.*)[G]';
+  [robname_kin, ~] = regexp(content,expression_robname2, 'tokens','match');
+  if ~isempty(robname_kin)
+    pkm_kin = robname_kin{1}{1};
+  else
+    pkm_kin = content;
+  end
   filename = 'writelock_mex.lock';
+  filedir = fullfile(repopath, folder, pkm_kin);
   lockfile = fullfile(filedir, filename);
+  ressource_name = pkm_kin;
 else
   error('Fall content=%s nicht definiert', content);
 end
 %% Sperrung aufheben (falls gefordert)
 if strcmp(mode, 'free')
   if verbosity
-    fprintf('Ressource %s/%dT%dR ist wieder frei.\n', content, sum(EEFG(1:3)), sum(EEFG(4:6)));
+    fprintf('Ressource %s ist wieder frei.\n', ressource_name);
   end
   delete(lockfile);
   return
@@ -74,15 +87,14 @@ while true
     % Sperrdatei existiert. Warte ab, bis Sperre vorbei ist
     if (now()-locktime)*24*3600 > patience
       if verbosity
-        fprintf(['Es wurde %1.1fs auf die Ressource %s/%dT%dR gewartet. ', ...
+        fprintf(['Es wurde %1.1fs auf die Ressource %s gewartet. ', ...
           'Ignoriere Schreibsperre mit Zeitstempel %s.\n'], ...
-          toc(t_start), content, sum(EEFG(1:3)), sum(EEFG(4:6)), ...
-          datestr(locktime,'yyyy-mm-dd HH:MM:SS'));
+          toc(t_start),ressource_name, datestr(locktime,'yyyy-mm-dd HH:MM:SS'));
       end
       break;
     elseif verbosity
-      fprintf('Ressource %s/%dT%dR ist gesperrt (Zeitstempel %s). Warte ab bis sie frei wird.\n', ...
-        content, sum(EEFG(1:3)), sum(EEFG(4:6)), datestr(locktime,'yyyy-mm-dd HH:MM:SS'));
+      fprintf('Ressource %s ist gesperrt (Zeitstempel %s). Warte ab bis sie frei wird.\n', ...
+        ressource_name, datestr(locktime,'yyyy-mm-dd HH:MM:SS'));
     end
     pause(2+10*rand(1,1)); % Wartezeit bis zur nächten Prüfung.
   else
@@ -105,5 +117,5 @@ end
 fprintf(fid, '%s', datestr(now,'yyyy-mm-dd HH:MM:SS'));
 fclose(fid);
 if verbosity
-  fprintf('Ressource %s/%dT%dR wurde gesperrt.\n', content, sum(EEFG(1:3)), sum(EEFG(4:6)));
+  fprintf('Ressource %s wurde gesperrt.\n', ressource_name);
 end
