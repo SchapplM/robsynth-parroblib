8f247d66751bb1e88037d59be1a5584a95970963
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2021-01-16 20:02:59 +0100

    Eintragen der technischen Gelenke bei der Initialisierung der Roboterklasse

--- parroblib_create_robot_class.m
+++ parroblib_create_robot_class.m
@@ -59,6 +59,45 @@ end
 if ~isempty(phi_RS_EE)
   RS.update_EE([], phi_RS_EE);
 end
+
+%% Technische Gelenke in Beinkette eintragen
+% Das wird nicht direkt beim Erstellen der seriellen Beinkette gemacht, da
+% dort noch nicht bekannt ist, ob die serielle Kette ein serieller Roboter
+% oder eine PKM-Beinkette ist. Siehe serroblib_gen_bitarrays.m.
+% Laden der Seriellroboter-Datenbank und extrahieren der Information:
+N = str2double(LEG_Names{1}(2));
+mdllistfile_Ndof = fullfile(fileparts(which('serroblib_path_init.m')), ...
+  sprintf('mdl_%ddof', N), sprintf('S%d_list.mat',N));
+l = load(mdllistfile_Ndof, 'Names_Ndof', 'AdditionalInfo');
+I_robot = find(strcmp(l.Names_Ndof,LEG_Names{1}));
+joints_number_str = sprintf('%d', l.AdditionalInfo(I_robot,7));
+numtechjoints = l.AdditionalInfo(I_robot,6);
+i_coord = 0; % Zähler über Gelenke im Modell
+for i = 1:numtechjoints % über technische Gelenke (können mehrwertig sein)
+  % Der Gelenktyp ist rückwärts gelesen kodiert (einer-Stelle ist erstes
+  % Gelenk).
+  joint_i = str2double(joints_number_str(end-i+1));
+  switch joint_i % 1=R,2=P,3=C,4=U,5=S
+    case 1 % R% 0=Drehgelenk, 1=Schub- (allgemein), 2=Kardan-, 3=Kugel
+      i_coord = i_coord + 1;
+      RS.DesPar.joint_type(i_coord) = 0; % Drehgelenk
+    case 2 % P
+      i_coord = i_coord + 1;
+      RS.DesPar.joint_type(i_coord) = 1; % Schubgelenk (allgemein)
+    case 3 % C
+      i_coord = i_coord + 2;
+      % Dreh-Schubgelenk noch nicht implementiert. Setze einfach direkt die
+      % MDH-Parameter. Keine Auswirkung.
+      RS.DesPar.joint_type(i_coord-1:i_coord) = RS.MDH.sigma(i_coord-1:i_coord);
+      warning('Gelenk Typ C noch nicht implementiert');
+    case 4 % U
+      i_coord = i_coord + 2;
+      RS.DesPar.joint_type(i_coord-1:i_coord) = 2; % Kardan
+    case 5 % S
+      i_coord = i_coord + 3;
+      RS.DesPar.joint_type(i_coord-2:i_coord) = 3; % Kugel
+  end
+end
 %% Instanz der parallelen Roboterklasse erstellen
 
 parroblib_addtopath({Name})
