c74f2687347721efb84b65ed31feced71b2f4ec0
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2023-04-01 15:17:44 +0200

    Skript schneller gemacht und auch Synthese-Ergebnisliste bereinigen

--- scripts/remove_pkm_missing_legs.m
+++ scripts/remove_pkm_missing_legs.m
@@ -4,10 +4,14 @@
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2019-02
 % (C) Institut für Mechatronische Systeme, Universität Hannover
 
+% Falls direkt vorher die Beinketten entfernt wurden:
+serroblib_gen_bitarrays();
+
 clc
 clear
 
 serroblibpath=fileparts(which('serroblib_path_init.m'));
+parroblibpath=fileparts(which('parroblib_path_init.m'));
 EEFG_Ges = logical(...
   [1 1 0 0 0 1; 1 1 1 0 0 0;  1 1 1 0 0 1; ...
    1 1 1 1 1 0; 1 1 1 1 1 1]);
@@ -16,7 +20,7 @@ for j = 1:size(EEFG_Ges,1)
 
   for i = 1:length(PNames_Kin)
     fprintf('%d/%d: Prüfe PKM %s\n', i, length(PNames_Kin), PNames_Kin{i});
-    [~, LEG_Names] = parroblib_load_robot([PNames_Kin{i},'A0']);
+    [~, LEG_Names] = parroblib_load_robot([PNames_Kin{i},'A0'], 0);
     % Prüfe, ob Name der Beinkette in Modellbibliothek serieller Roboter ist
     NFG_Leg = str2double(LEG_Names{1}(2));
     mdllistfile_Ndof = fullfile(serroblibpath, sprintf('mdl_%ddof', NFG_Leg), sprintf('S%d_list.mat',NFG_Leg));
@@ -29,6 +33,26 @@ for j = 1:size(EEFG_Ges,1)
     success = parroblib_remove_robot(PNames_Kin{i});
     if ~success
       error('Fehler beim Löschen von %s', PNames_Kin{i});
+    end
+  end
+  % Gehe durch Liste der Synthese-Ergebnisse
+  for k = 1:2
+    if k == 1, varstr = '';
+    else,      varstr = '_var';
+    end
+    synthresfile = fullfile(parroblibpath, 'synthesis_result_lists', ...
+      sprintf('%dT%dR%s.csv', sum(EEFG_Ges(j,1:3)), sum(EEFG_Ges(j,4:6)), varstr));
+    synthrestab = readtable(synthresfile);
+    I_del = false(size(synthrestab,1), 1);
+    for i = 1:size(synthrestab,1)
+      if ~any(strcmp(l.Names_Ndof, synthrestab.Beinkette{i}))
+        I_del(i) = true;
+      end
+    end
+    fprintf('Lösche %d Einträge aus Tabelle %s\n', sum(I_del), synthresfile);
+    if sum(I_del) > 0
+      synthrestab = synthrestab(~I_del,:);
+      writetable(synthrestab, synthresfile, 'Delimiter', ';');
     end
   end
 end
